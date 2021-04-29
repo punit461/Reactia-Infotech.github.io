f6948651c4e6af054b8ff931f5b9a0f15447033e
punit bhardwaj <punit461bhardwaj@gmail.com>
2021-04-29 11:45:52 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,34 +1,20 @@
 <?php 
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
 
-  $receiving_email_address = 'punit461bhardwaj@gmail.com';
 
-  // if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-  //   include( $php_email_form );
-  // } else {
-  //   die( 'Unable to load the "PHP Email Form" Library!');
-  // }
+    $email_from = 'Reactia Infotech';
+    $email_subject ="from Reactia Infotech Contact: $subject";
+    $email_body = "Name: $name.\n".
+                  "Email:$email.\n".
+                  "Message: $message.\n";
+    
+    $to ="punit461bhardwaj@gmail.com";
+    $headers = "From: $email_from \r\n";
+    $headers .= "Reply-To: $email \r\n";
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+    mail($to,$email_subject,$email_body,$headers);
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
 ?>
